21f92cbc839ecdba684db6e9408cda407510b5ff
Yura931 <dbfk931@naver.com>
2021-05-26 00:32:43 +0900

    20210526

---
+++ src/baekjoon_algorhytm/month05/Baek_1271.java
@@ -0,0 +1,20 @@
+package baekjoon_algorhytm.month05;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Baek_1271 {
+	public static void main (String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		// 생명체 수
+		BigInteger a = sc.nextBigInteger();
+		
+		// 분배할 돈
+		BigInteger b = sc.nextBigInteger();
+        
+		System.out.println(a.divide(b));
+		System.out.println(a.mod(b)); 
+		
+	}
+}
